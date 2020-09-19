4a0563832db82a7abc998d8fa91fb29a3e9f7d5f
takumi.yamaoka <tk-y@docomo.ne.jp>
2020-09-19 09:42:36 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by 山岡巧 on 2020/06/01.
+//  Copyright © 2020 takumi.yamaoka. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // SKViewに肩を変換する
+        let skView = self.view as! SKView
+        
+        // FPSを表示する
+        skView.showsFPS = true
+        
+        // ノードの数を表示する
+        skView.showsNodeCount = true
+        
+        // ビューと同じサイズでシーンを作成する
+        let scene  = GameScene(size:skView.frame.size)
+        
+        // ビューにシーンを表示する
+        skView.presentScene(scene)
+    }
+    
+    // ステータスバーを消す
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
+    }
+
+
+}
+
